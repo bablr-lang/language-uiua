e85b3ecd81b3535b7a5c48dff7eb12481dc436f4
Stirling Hostetter <stirlhoss@proton.me>
2024-06-19 11:40:33 -0500

    start working on the uiua grammar

--- test/grammar.test.js
+++ test/grammar.test.js
@@ -10,11 +10,11 @@ let enhancers = undefined;
 // enhancers = debugEnhancers;
 
 const uiua = (...args) =>
-	printPrettyCSTML(buildTag(language, 'Expression', undefined, enhancers)(...args));
+  printPrettyCSTML(buildTag(language, 'Expression', undefined, enhancers)(...args));
 
 describe('@bablr/language-uiua', () => {
-	it('`"hello"`', () => {
-		expect(uiua`"hello"`).toEqual(dedent`\
+  it('`"hello"`', () => {
+    expect(uiua`"hello"`).toEqual(dedent`\
       <!0:cstml bablr-language='https://github.com/bablr-lang/language-uiua'>
       <>
         <String>
@@ -26,7 +26,21 @@ describe('@bablr/language-uiua', () => {
           closeToken: <~*Punctuator '"' balancer />
         </>
       </>\n`);
-	});
+  });
 
+  it('`[1 2 3]`', () => {
+    console.log(uiua`[1 2 3]`);
+    expect(uiua`[1 2 3]`).toEqual(dedent`\
+      <!0:cstml bablr-language='https://github.com/bablr-lang/language-uiua'>
+      <>
+        <String>
+          openToken: <~*Punctuator '"' balanced='"' balancedSpan='String' />
+          content:
+          <*StringContent>
+            'hello'
+          </>
+          closeToken: <~*Punctuator '"' balancer />
+        </>
+      </>\n`);
+  });
 });
-
